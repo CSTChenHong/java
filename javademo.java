035c444af53f927fbb0e980facda46ddc218a185
CSTChenHong <39369946+CSTChenHong@users.noreply.github.com>
2018-11-30 10:16:39 +0800

    Add files via upload

---
+++ javademo.java
@@ -0,0 +1,9 @@
+package java1;
+
+public class javademo {
+	public javademo(String name) {
+		System.out.println("小狗的名字是："+name);
+	}
+	public static void main(String[] args)
+	{javademo myjavademo=new javademo("sunny");}
+}
